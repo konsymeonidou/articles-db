e0191a5fc802c14734a978f54a72fdf0ce096a06
konsymeonidou <symeonidou.kon@gmail.com>
2025-02-22 18:29:41 +0200

    wip

--- articlesdb/api/tests.py
+++ articlesdb/api/tests.py
@@ -1,23 +1,30 @@
 from django.test import TestCase
 from django.test import TestCase
 from django.contrib.auth.models import User
-from .models import Article, Tag, Comment
-from .views import ArticleViewSet, TagViewSet, CommentViewSet
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from .models import Article, Tag, Comment, Author
+from datetime import date
 
 # add more tests here
 
 class TestModels(TestCase):
     def setUp(self):
-        self.author = User.objects.create(username='test_user', password='test')
+        self.user = User.objects.create(username='test_user', password='test')
+        self.author = Author.objects.create(name='test_author')
         self.tag = Tag.objects.create(name='test_tag')
 
+        self.article = Article.objects.create(
+            identifier="12345",
+            publication_date=date.today(),
+            abstract="This is an abstract.",
+            title="Sample Article"
+        )
+        self.article.authors.add(self.author)
+        self.article.tags.add(self.tag)
+
     def test_model_article(self):    
-        article_item = Article.objects.create(title='test_article', abstract='test_abstract')
-        article_item.authors.add(self.author)
-        article_item.tags.add(self.tag)
+        article_item=self.article
         article_item.save()
-        self.assertEqual(str(article_item), 'test_article')
+        self.assertEqual(str(article_item), 'Sample Article')
         self.assertTrue(isinstance(article_item, Article))
 
     def test_model_tag(self):
@@ -27,11 +34,9 @@ class TestModels(TestCase):
         self.assertTrue(isinstance(tag_item, Tag))
 
     def test_model_comment(self):
-        article_item = Article.objects.create(title='test_article', abstract='test_abstract')
-        article_item.authors.add(self.author)
-        article_item.tags.add(self.tag)
+        article_item = self.article
         article_item.save()
         print(article_item)
-        comment_item = Comment.objects.create(user=self.author, article=article_item, text='test_text')
-        self.assertEqual(str(comment_item), 'Comment by test_user on test_article')
+        comment_item = Comment.objects.create(user=self.user, article=article_item, text='test_text')
+        self.assertEqual(str(comment_item), 'Comment by test_user on Sample Article')
         self.assertTrue(isinstance(comment_item, Comment))
